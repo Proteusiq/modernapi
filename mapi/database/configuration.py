4980b1227bbba07492ae64b604546d6657c9e0db
Prayson Wilfred Daniel <praysonpi@gmail.com>
2021-10-07 19:46:47 +0200

    Update configuration.py

--- mapi/database/configuration.py
+++ mapi/database/configuration.py
@@ -7,13 +7,56 @@ DATABASE_URL = config("DATABASE_URI", default="sqlite:///database.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
-SQLModel.metadata.create_all(engine)
+hero_1 = UserInDB(
+    username="Deadpond",
+    email="deadpond@example.com",
+    full_name="Dive Wilson",
+    hashed_password="gogo",
+)
+hero_2 = UserInDB(
+    username="Spider-Boy",
+    email="spider@example.com",
+    full_name="Pedro Parqueador",
+    hashed_password="$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+)
 
 
-def get_db():
 
-    db = Session(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+create_db_and_tables()
+
+# with Session(engine) as session:
+
+#     session.add(hero_1)
+#     session.add(hero_2)
+#     session.commit()
+
+# with Session(engine) as session:
+
+#     selection = select(UserInDB).where(UserInDB.username == "Spider-Boy")
+#     result = session.exec(selection).first()
+
+#     print(result)
+
+
+def get_user(username: str):
+
+    with Session(engine) as session:
+
+        selection = select(UserInDB).where(UserInDB.username == username)
+        user = session.exec(selection).first()
+
+        return user
+
+
+
+def get_session():
+
+    session = Session(engine)
     try:
-        yield db
+        yield session
     finally:
-        db.close()
+        session.close()
