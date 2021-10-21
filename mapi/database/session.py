2b52e211032e504f2f64050e3123b27ae0be3caf
Prayson W. Daniel <praysonwilfred@gmail.com>
2021-10-21 10:13:49 +0200

    created shared engine

--- mapi/database/session.py
+++ mapi/database/session.py
@@ -1,22 +1,23 @@
 from typing import Dict, Literal, Union
 from decouple import config
+from sqlalchemy import engine
 from sqlmodel import Session, create_engine, select
 from models.user import UserInDB, User
 from schemas.user import UserCreate
 from core.password import get_password_hash
 
 
-def get_connection(mode:Literal["dev", "prod"]="prod") -> Session:
+DATABASE_URL = config("DATABASE_URI", default="sqlite:///database.db")
+DEBUG_MODE= config("DEBUG_STAGE", default="prod")
 
-    DATABASE_URL = config("DATABASE_URI", default="sqlite:///database.db")
-    if mode == "dev":
-        return create_engine(DATABASE_URL, echo=True)
-    return create_engine(DATABASE_URL,  connect_args={"sslmode": "required"}, echo=False)
+engine = create_engine(DATABASE_URL,  connect_args={"sslmode": "required"}, echo=False)
+if DEBUG_MODE == "dev":
+    engine = create_engine(DATABASE_URL, echo=True)
+
 
 
 def get_user(username: str) -> UserInDB:
 
-    engine = get_connection(mode=config("DEBUG_STAGE", default="prod"))
     with Session(engine) as session:
 
         selection = select(UserInDB).where(UserInDB.username == username)
@@ -26,7 +27,6 @@ def get_user(username: str) -> UserInDB:
 
 def get_user_role(username: str) -> Union[str, None]:
 
-    engine = get_connection(mode=config("DEBUG_STAGE", default="prod"))
     with Session(engine) as session:
         selection = select(UserInDB).where(
             UserInDB.username == username
@@ -40,7 +40,6 @@ def get_user_role(username: str) -> Union[str, None]:
 
 def get_users() -> User:
 
-    engine = get_connection(mode=config("DEBUG_STAGE", default="prod"))
     with Session(engine) as session:
 
         selection = select(User)
@@ -50,7 +49,7 @@ def get_users() -> User:
 
 
 def delete_user(username: str) -> Dict[str, bool]:
-    engine = get_connection(mode=config("DEBUG_STAGE", default="prod"))
+    
     with Session(engine) as session:
         selection = select(UserInDB).where(UserInDB.username == username
         )
@@ -66,7 +65,7 @@ def delete_user(username: str) -> Dict[str, bool]:
 
 
 def create_user(user: UserCreate) -> Dict[str, bool]:
-    engine = get_connection(mode=config("DEBUG_STAGE", default="prod"))
+    
     with Session(engine) as session:
 
         # check if user exists
